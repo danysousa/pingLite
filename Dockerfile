09c3fdad8abb1609ee610e45727c9df85a3372b1
Dany SOUSA <dsousa@e3r7p3.42.fr>
2015-12-03 11:19:39 +0100

    [ADD] launch with shared folder

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,2 @@
 FROM debian:7
 RUN apt-get -y update && apt-get -y install build-essential git
-
--- launch.sh
+++ launch.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-docker run -it -v $PWD/home:/home/ debian-custom
+#docker run -it -v $PWD/home:/home/ debian-custom
+
+grp="2013_paris"
+
+if [ "$grp" = "$GROUP" ];
+then
+	path=${PWD/$HOME/'/Users/dsousa'}
+	docker run -it -v $path/home:/home/ debian-custom
+else
+	docker run -it -v $PWD/home:/home/ debian-custom
+fi
